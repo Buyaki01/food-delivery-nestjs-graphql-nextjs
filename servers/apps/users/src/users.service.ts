d9c0b29d0a97b366c0a71fd2cecc74238bc714d2
ritta.sweta <ritta.sweta@savannahinformatics.com>
2024-07-18 13:33:20 +0300

    Setup prisma and mongoDB

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,26 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto, RegisterDto } from './dto/user.dto';
 import { Response } from 'express';
+import { PrismaService } from '../../../prisma/Prisma.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     private readonly jwtService: JwtService,
-    // private readonly prisma:
+    private readonly prisma: PrismaService,
     private readonly configService: ConfigService,
   ) {}
 
   // Register User service
   async register(registerDto: RegisterDto, response: Response) {
     const { name, email, password } = registerDto;
-    const user = {
-      name,
-      email,
-      password,
-    };
-    return user;
+
+    const isEmailExist = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (isEmailExist) {
+      throw new BadRequestException('User already exist with this email!');
+    }
+
+    const user = await this.prisma.user.create({
+      data: { name, email, password },
+    });
+    return { user, response };
   }
 
   // Login Service
@@ -36,7 +44,6 @@ export class UsersService {
 
   // get all users service
   async getUsers() {
-    const users = [];
-    return users;
+    return this.prisma.user.findMany({});
   }
 }
